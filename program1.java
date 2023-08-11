db35476f0f93870ea80b69f7a1c0562d5735420b
Rohan Ash <97527130+Rohan-ashish@users.noreply.github.com>
2023-08-11 11:38:20 +0530

    Create program1.java
    program to add two int

---
+++ program1.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        int a = 9;
+        int b = 7;
+        int c = a +b;
+        System.out.println(c);
+    }
+}
